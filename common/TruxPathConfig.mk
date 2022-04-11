53ef64fdeff567aefb2c2be8a8280164d256aac8
Sawan Roy <sawan.roy@trunexa.com>
2022-04-11 17:44:57 +0530

    imx8mm-trucrux:machine build support added

---
+++ common/TruxPathConfig.mk
@@ -0,0 +1,26 @@
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+BCM_FIRMWARE_PATH := vendor/trucrux/bcm_4343w_fw
+KERNEL_IMX_PATH := vendor/trucrux
+UBOOT_IMX_PATH := vendor/trucrux
+FSL_PROPRIETARY_PATH := vendor/nxp
+LINUX_FIRMWARE_PATH := vendor/nxp
+FSL_CODEC_PATH := vendor/nxp
+FSL_IMX_OMX_PATH := vendor/nxp
+IMX_FIRMWARE_PATH := vendor/nxp
+IMX_G2D_PXP_PATH := vendor/nxp
+IMX_VPU_CNM_PATH := vendor/nxp
+IMX_VPU_HANTRO_PATH := vendor/nxp
+LINUX_FIRMWARE_IMX_PATH := vendor/nxp
+IMX_MCU_SDK_PATH := vendor/nxp
+IMX_ANDROID_MM_PATH := vendor/nxp
+IMX_MEDIA_CODEC_XML_PATH:= vendor/nxp/imx_android_mm
+
+IMX_LIB_PATH := vendor/nxp-opensource
+IMX_MKIMAGE_PATH := vendor/nxp-opensource
+IMX_PATH := vendor/nxp-opensource
+FSL_IMX_DEMO_PATH := vendor/nxp-opensource
+LIBDRM_IMX := vendor/nxp-opensource
---
+++ imx8m/imx8mm_trux_q01/UbootKernelBoardConfig.mk
@@ -0,0 +1,22 @@
+TARGET_BOOTLOADER_POSTFIX := bin
+UBOOT_POST_PROCESS := true
+
+TARGET_BOOTLOADER_CONFIG := \
+	imx8mm-trux-q01:imx8mm_trux_android_defconfig \
+        imx8mm-trux-q01-uuu:imx8mm_trux_android_uuu_defconfig
+
+# imx8mm kernel defconfig
+ifneq ($(IMX8MM_USES_GKI),)
+TARGET_KERNEL_DEFCONFIG := gki_defconfig
+TARGET_KERNEL_GKI_DEFCONF:= android_gki_defconfig
+else
+TARGET_KERNEL_DEFCONFIG := imx8_trux_android_defconfig
+endif
+TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
+
+# absolute path is used, not the same as relative path used in AOSP make
+TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+# define bootloader rollback index
+BOOTLOADER_RBINDEX ?= 0
+
